d48c8577eb9ae79d2a36c98ab4fcacb2e9b4d83a
Mansi <mansikashyap134@gmail.com>
2026-03-06 10:03:25 +0530

    Added CORS middleware to fix frontend-backend connection

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,10 +1,19 @@
 
 
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.schemas import PredictionRequest
 from app.services.prediction import predict_hospital_status
 
 app = FastAPI(title = "SwasthaSevaAI")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 def home():
